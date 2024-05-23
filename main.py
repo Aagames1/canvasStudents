224147bb1b4143ade4298ff683d73e17e43328b2
Adrian Sun <95660109+Aagames1@users.noreply.github.com>
2024-05-23 12:34:27 +0000

    Update main.py
    it works!

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from canvasapi import Canvas 
 from canvasapi.exceptions import CanvasException
-API_URL = "https://example.com"
-API_KEY = "p@$$w0rd" 
+API_URL = "https://mcpsmd.instructure.com/"
+API_KEY = "[insert key here]" # obviously needs to be changed when used
 canvas = Canvas(API_URL, API_KEY)
 
 def getStudents(courseID):
@@ -10,15 +10,17 @@ def getStudents(courseID):
   studentsList = []
   
   for user in users:
-    studentsList.append(user)
+    studentsList.append(user.name)
     
   return studentsList
 
 
+# this part will likely be changed to eliminate the need for manual input
 print("Enter the course ID of the course you want to obtain a list of all students from: ")
 ID = input()
 
 try:
-    getStudents(ID)
+    print(getStudents(ID))
 except CanvasException as e:
     print("An error has occured. Check to see if the ID was valid.")
+
