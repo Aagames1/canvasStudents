077774c13ae153a71fe3f1949130ee1ec52e2400
Adrian Sun <95660109+Aagames1@users.noreply.github.com>
2024-05-24 12:25:18 +0000

    .txt file now gives the name of the course on first line

--- main2.py
+++ main2.py
@@ -1,7 +1,8 @@
 from canvasapi import Canvas 
 from canvasapi.exceptions import CanvasException
+import time
 API_URL = "https://mcpsmd.instructure.com/"
-API_KEY = "[INSERT KEY HERE]" 
+API_KEY = "2873~KtJk8NXGQ9uL8chnTCT7xtJk88ZWUuWUKMN6zkxWXcTPTuZBQENuhU3mBQxttwrA" 
 canvas = Canvas(API_URL, API_KEY)
 
 def getStudents(courseID):
@@ -16,6 +17,9 @@ def getStudents(courseID):
     
   return studentsList
 
+def getCourseName(courseID):
+  course = canvas.get_course(courseID)
+  return course.name
 
 while True:
   print("\nEnter the course ID of the course you want to obtain a list of all students from: ")
@@ -25,6 +29,8 @@ while True:
     nameList = getStudents(ID)
     
     with open('output.txt', 'w') as file:
+      file.write(getCourseName(ID))
+      file.write('\n')
       for item in nameList:
           file.write('%s\n' % item)
         
